874931c59d10fad9754b4cfce89a03de46991e5b
Shadow584 <55329280+Shadow584@users.noreply.github.com>
2020-09-23 16:46:08 -0300

    Criando rota para criação de Todo

--- src/app/routes/todos.ts
+++ src/app/routes/todos.ts
@@ -1,7 +1,31 @@
 import { Router } from '@awaitjs/express'
+import { Request, Response } from 'express'
+import { AppCore } from '../AppCore'
+import { CoreError } from '../../core/entities/CoreError';
+import { authenticateRequest } from '../middlewares/authenticateRequest'
 
-const router = Router()
+const router = Router();
 
-// TODO: Rotas Todo
+router.postAsync(
+    '/',
+    authenticateRequest,
+    async (req: Request, res: Response): Promise<Response> => {
+        const { description, name } = req.body;
+        const authTokenData = req.tokenData;
+        
+        try {
+            const todo = await AppCore.createTodo(authTokenData, {
+                description,
+                shortName: name
+            });
+            return res.status(201).json(todo)
+        } catch (err) {
+            if (err instanceof CoreError)
+                return res.status(400).send();
+            else
+                return res.status(500).send();
+        }
+    }
+)
 
 export { router }
