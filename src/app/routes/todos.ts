6f42dd117219fa222dbcd8b9f4ecc2924451e109
Shadow584 <55329280+Shadow584@users.noreply.github.com>
2020-09-23 17:17:18 -0300

    Adicionando rota para edição de Todo

--- src/app/routes/todos.ts
+++ src/app/routes/todos.ts
@@ -1,10 +1,21 @@
 import { Router } from '@awaitjs/express'
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
+import { CreateTodoData } from '../../core';
 import { AppCore } from '../AppCore'
-import { CoreError } from '../../core/entities/CoreError';
 import { authenticateRequest } from '../middlewares/authenticateRequest'
 
 const router = Router();
+
+router.getAsync(
+    '/',
+    authenticateRequest,
+    async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+        const authTokenData = req.tokenData;
+        const todos = await AppCore.getTodos(authTokenData);
+
+        return res.status(200).json(todos);
+    }
+)
 
 router.postAsync(
     '/',
@@ -18,6 +29,22 @@ router.postAsync(
             shortName: name
         });
         return res.status(201).json(todo)
+    }
+)
+
+router.putAsync(
+    '/:id',
+    authenticateRequest,
+    async (req: Request, res: Response): Promise<Response> => {
+        const authTokenData = req.tokenData
+        const { id } = req.params
+        const { description, name } = req.body
+
+        const todoData: Partial<CreateTodoData> = { description, shortName: name }
+
+        await AppCore.updateTodo(authTokenData, id, todoData)
+
+        return res.sendStatus(200)
     }
 )
 
